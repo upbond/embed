f83608dfca5677a319d729ebc45989f77eb56a16
rashgaroth <50513263+rashgaroth@users.noreply.github.com>
2022-10-31 13:49:35 +0800

    add samples

---
+++ examples/react/src/App.js
@@ -0,0 +1,172 @@
+import { UpbondLogo } from "assets";
+import SpinnerLoading from "component/SpinnerLoading";
+import upbondServices from "lib/UpbondEmbed";
+import { useEffect, useState } from "react";
+import Web3 from "web3";
+
+/* 
+  Read this:
+  This concept actually can be also using hooks (functional), you may can decide what you want to do
+  if you're using hooks, sure you can put the new Upbond({}) on the useState.
+  We're using this example because we're usually using this method for implementing the @upbond/upbond-embed lib
+*/
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [account, setAccount] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
+  const [signInfo, setSignInfo] = useState(null)
+
+  const [upbondProvider, setUpbondProvider] = useState(null)
+
+  useEffect(() => {
+    const initUpbond = async () => {
+      setLoading(true)
+      try {
+        await upbondServices.init()
+      } catch (error) {
+        console.error(`Error initialization: `, error)
+      }
+      setLoading(false)
+    }
+    if (upbondServices.initialized) {
+      return
+    }
+    initUpbond()
+  }, [])
+
+  const login = async () => {
+    setLoading(true)
+    try {
+      const login = await upbondServices.login()
+      console.log(login, '@LOGIN???')
+      if (login.data !== null) {
+        console.log('Success login!')
+
+        setUpbondProvider(login.data)
+        setAccount(login.accounts)
+        setLoading(false)
+        return
+      }
+      console.log(`error login: ${login.msg}`)
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      console.error(error)
+      throw new Error(error)
+    }
+  };
+
+  const getUser = async () => {
+    setLoading(true);
+    try {
+      const getData = await upbondServices.getUserInfo();
+      setUserInfo(getData);
+
+      setLoading(false);
+    } catch (error) {
+      console.error(error, '@errorOnGetUser')
+      setLoading(false)
+    }
+  };
+
+  const signTransaction = async () => {
+    try {
+      const msgHash = Web3.utils.keccak256('Signing Transaction for Upbond Embed!')
+      const signedMsg = await upbondServices.signTransaction(msgHash, account[0])
+      console.log(signedMsg)
+      setSignInfo(signedMsg)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    if (upbondProvider !== null) {
+      if (upbondProvider.on) {
+        upbondProvider.on("accountsChanged", (accounts) => {
+          console.log(`Account changed: ${accounts}`)
+        })
+
+        upbondProvider.on("chainChanged", (res) => {
+          console.log(`Chain changed on: ${res}`)
+        })
+      }
+    }
+  }, [upbondProvider])
+
+  return (
+    <div className="mx-auto max-w-2xl sm:px-6 lg:px-8">
+      <header className="App-header">
+        <p className="text-center uppercase font-bold my-3">
+          sample dapps with upbond embed
+        </p>
+        <img src={UpbondLogo} className="w-1/2 mx-auto" alt="UpbondBanner" />
+        {account ? (
+          <>
+            <div>
+              <p className="text-center">Account : {account}</p>
+
+              <div className="flex justify-center mt-3 gap-3">
+                <button
+                  type="button"
+                  className="items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  onClick={getUser}
+                >
+                  Get User Info
+                </button>
+              </div>
+              {/* User Info */}
+              {userInfo && (
+                <div className="text-center my-3">
+                  <p className="font-bold">User Info</p>
+                  <p>Name: {userInfo.name}</p>
+                  <p>Email: {userInfo.email}</p>
+                </div>
+              )}
+              <div className="flex flex-1 justify-center space-x-5 mt-2">
+                <button
+                  type="button"
+                  className="items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                  onClick={signTransaction}
+                >
+                  Sign Transaction
+                </button>
+                <button
+                  type="button"
+                  className="items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                  onClick={async () => await upbondServices.logout()}
+                >
+                  Logout
+                </button>
+              </div>
+              <div className="flex justify-center mt-3 gap-3">
+                {
+                  signInfo !== null && (
+                    <p className="text-black">Signed with sign: {signInfo}</p>
+                  )
+                }
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="flex justify-center">
+            {loading === true ? (
+              <SpinnerLoading />
+            ) : (
+              <button
+                type="button"
+                className="mx-auto px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500  w-1/4"
+                onClick={login}
+              >
+                Login
+              </button>
+            )}
+          </div>
+        )}
+      </header>
+    </div>
+  );
+};
+
+export default App;
---
+++ examples/react/src/lib/UpbondEmbed.js
@@ -0,0 +1,110 @@
+import Upbond, { UPBOND_BUILD_ENV } from "@upbond/upbond-embed";
+import Web3 from "web3";
+
+class UpbondEmbed {
+
+  // Initials
+  upbond = null
+
+  web3 = null
+
+  // you can also using another envs.
+  env = UPBOND_BUILD_ENV.TESTING
+  
+  provider
+
+  isLoggedIn = false
+
+  initialized = false
+
+  constructor() {
+    this.upbond = new Upbond({})
+    this.web3 = new Web3()
+    this.provider = null
+  }
+
+  async init() {
+    if (this.upbond instanceof Upbond) {
+      await this.upbond.init({
+        buildEnv: this.env
+      })
+      this.initialized = true
+    }
+  }
+
+  async login() {
+    try {
+      if (this.upbond instanceof Upbond && this.web3 instanceof Web3) {
+        const _provider = await this.upbond.login() // login using upbond
+        console.log(_provider, '@upbondProvider?')
+        console.log(this.upbond.provider, '@provider?')
+        this.web3.setProvider(this.upbond.provider)
+
+        const accounts = await this.web3.eth.getAccounts()
+        console.log(`Provided account: ${accounts[0]}`)
+
+        this.isLoggedIn = true
+        this.provider = _provider
+        return {
+          msg: 'success',
+          data: _provider,
+          accounts
+          // ... anything that you want to returns
+        }
+      }
+    } catch (error) {
+      return {
+        msg: error.message || 'Failed to login',
+        data: null
+      }
+    }
+  }
+
+  async logout() {
+    try {
+      if (this.upbond instanceof Upbond) {
+        await this.upbond.logout()
+        await this.upbond.cleanUp()
+        window.location.reload()
+
+        return {
+          msg: 'success',
+          data: true
+        }
+      }
+    } catch (error) {
+      return {
+        msg: error.message || 'Failed to login',
+        data: null
+      }
+    }
+  }
+
+  async getUserInfo() {
+    if (this.upbond instanceof Upbond && this.isLoggedIn) {
+      try {
+        const userInfo = await this.upbond.getUserInfo()
+        return userInfo
+      } catch (error) {
+        throw new Error(error)
+      }
+    }
+  }
+
+  async signTransaction(msg = "", account) {
+    if (this.web3 instanceof Web3) {
+      try {
+        this.web3.setProvider(this.upbond.provider)
+        const sign = await this.web3.eth.sign(msg, account)
+        return sign
+      } catch (error) {
+        console.error(error)
+        return null
+      }
+    }
+  }
+}
+
+const upbondServices = new UpbondEmbed()
+
+export default upbondServices
